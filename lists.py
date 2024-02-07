57c5747bb87bd8eea4e277a7d82c5a603049cc0b
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-02-07 13:57:32 -0500

    Update lists.py

--- lists.py
+++ lists.py
@@ -4,22 +4,21 @@ if (x%2) > 0:
     print("odd")
 else:
     print("even") """
-# CHallnege2
-# x = (int(input("What are all the factors of this number?")))
-# if (x%2)
 
-#  def get_all_factors(n)
-#         factors = []
-#         for  i in range(1 + n+1)
-#         if n%i == 0
-#         factors.append(i)
-y = int(input("What is the bill?"))
-service = input("how was the service? ")
-if service == "bad":
-    print(y)
-if service == "okay":
-    print(y + (y%15))
-if service == "good":
-    print(y + (y%20))
-if service == "great":
-    print(y + (y%25))
+# Challenge 2: bill calculator
+# y = int(input("What is the bill?"))
+# service = input("how was the service? ")
+# if service == "bad":
+#     print(y)
+# if service == "okay":
+#     print(y + (y%15))
+# if service == "good":
+#     print(y + (y%20))
+# if service == "great":
+#     print(y + (y%25))
+
+def get_all_factors(n):
+        factors = []
+        for  i in range(1 + n+1):
+            if n%i == 0:
+                factors.append(i)

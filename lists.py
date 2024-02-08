5cb9b8ba6735074ebb577ccf671e9cf1f3c01a5f
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-02-08 14:00:30 -0500

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,24 +1,30 @@
-# Challenge 1
+""" """ # Challenge 1
 """ x = (int(input("Is it even or odd?")))
 if (x%2) > 0:
     print("odd")
 else:
     print("even") """
 
-# Challenge 2: bill calculator
-# y = int(input("What is the bill?"))
-# service = input("how was the service? ")
-# if service == "bad":
-#     print(y)
-# if service == "okay":
-#     print(y + (y%15))
-# if service == "good":
-#     print(y + (y%20))
-# if service == "great":
-#     print(y + (y%25))
 
-def get_all_factors(n):
-        factors = []
-        for  i in range(1 + n+1):
-            if n%i == 0:
-                factors.append(i)
+""" y = int(input("What is the bill?"))
+service = input("how was the service? ")
+if service == "bad":
+    print(y)
+if service == "okay":
+    print(y + (y%15))
+if service == "good":
+    print(y + (y%20))
+if service == "great":
+    print(y + (y%25)) """
+
+
+def allfactors(n):
+    factors = []
+    for i in range(1,n+1):
+        if n%i == 0:
+            factors.append(i)
+        return factors
+number = int(input("Please enter a number: "))
+listfactors = allfactors(number)
+print(listfactors)
+
